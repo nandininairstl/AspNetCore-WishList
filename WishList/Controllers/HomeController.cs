7f33fb0801f3ee50ebdc60a7fd8075c36be38a23
nandininairstl <nandini.nair@stl.tech>
2022-07-30 11:49:55 +0530

    Create Home Views and the HomeController

---
+++ WishList/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WishList.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Views/Index.cshtml");
+        }
+        public IActionResult Error()
+        {
+            return View("~/Views/Shared/Error.cshtml");
+        }
+    }
+}
