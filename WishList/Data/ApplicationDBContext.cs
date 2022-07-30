9f1e337a3365e2bd50a6ed4114776de8e6ea5951
nandininairstl <nandini.nair@stl.tech>
2022-07-30 12:49:43 +0530

    Update 4.0

--- WishList/Data/ApplicationDBContext.cs
+++ WishList/Data/ApplicationDBContext.cs
@@ -6,7 +6,7 @@ namespace WishList.Data
     public class ApplicationDbContext : DbContext
     {
         public DbSet<Item> Items { get; set; }
-        ApplicationDbContext(DbContextOptions options) : base(options)
+        public ApplicationDbContext(DbContextOptions options) : base(options)
         {
 
         }
